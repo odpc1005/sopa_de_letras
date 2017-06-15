bdd7027b2ddaa086bbf4efccddb30dcdf0579352
David <odpc1005@gmail.com>
2017-06-14 21:23:42 -0500

    with random

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,13 @@ class WelcomeController < ApplicationController
 	
 	def index
 		#data preparations
-		
+		@sopa_arr = []
+		(0..8).each do |i|
+			@sopa_j = []
+			(0..8).each do |j|
+			 @sopa_j.push((65 + rand(26)).chr)
+			end
+			@sopa_arr.push(@sopa_j)
+		end
 	end
 end
